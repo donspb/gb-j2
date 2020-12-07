d2fd37330adee90119ac9f6332f51de793384eee
donspb <56118367+donspb@users.noreply.github.com>
2020-12-07 11:29:55 +0300

    Homework 3

--- Main.java
+++ Main.java
@@ -1,50 +1,60 @@
-package geekbrains.jt.homework1;
+package geekbrains.jt.homework2;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
 
-    private static final int MATRIX_SIZE = 4;
+    static String words = "качели степь Буг ананас вазон куб закладка аттракцион степь тумба кошка дипломат степь колдобина кошка" +
+            " ушиб коктейль монтировка степь куб кошка контейнер кошка бугор щеколда степь баржа корж кошка киль куб ананас " +
+            "конечность планшет степь лесопункт пленка хризантема Буг ананас дипломат коляска кошка степь амулет";
 
     public static void main(String[] args) {
-        try {
-            System.out.println(converterArrayInt(converterStringArray("10 3 1 2\n2 3 2 2\n5 6 7 1\n300 3 1 0")));
-        } catch (ArraySizeException e) {
-            System.out.println(e.getMessage());
-            System.out.println(e.getMoreDetails());
-        } catch (ArrayDataException e) {
-            System.out.println(e.getMessage());
-            System.out.println(e.getMoreDetails());
+        String[] wordsArray = words.split(" ");
+        ArrayList<String> wordsList = new ArrayList<>();
+        HashMap<String, Integer> wordsMap = new HashMap<>();
+
+        for (int i = 0; i < wordsArray.length; i++) {
+            if (!wordsList.contains(wordsArray[i])) wordsList.add(wordsArray[i]);
+            if (wordsMap.containsKey(wordsArray[i])) wordsMap.replace(wordsArray[i],wordsMap.get(wordsArray[i]) + 1);
+            else wordsMap.put(wordsArray[i], 1);
+        }
+
+        System.out.println("Список слов:");
+        System.out.println(wordsList);
+        System.out.println("Количество слов:");
+        System.out.println(wordsMap);
+
+        /* -------------------------------------------------------- */
+
+        PhoneBook phoneBook = new PhoneBook();
+
+        phoneBook.addContact("Ivanov", "+79217029485", "ivanov_only@email.com");
+        phoneBook.addContact("Ivanov", "+79052019901", "ivanov_clone@email.net");
+        phoneBook.addContact("Petrov", "+79114163721", "petrov_mail@email.org");
+        phoneBook.addContact("Sidorov", "+79048111000", "sidorov@email.in");
+        phoneBook.addContact("Kozlov", "+79019010109", "superman@email.to");
+        phoneBook.addContact("Ivanov", "+79317004359", "ivanov_ivanov@email.us");
+
+        String surnameToSearch = "Johnson";
+
+        String[] searchResult = phoneBook.getContactPhone(surnameToSearch);
+        System.out.println("Результат поиска телефонов по фамилии " + surnameToSearch + ":");
+        printResult(searchResult);
+
+        searchResult = phoneBook.getContactEmail(surnameToSearch);
+        System.out.println("Результат поиска адресов эл. почты по фамилии " + surnameToSearch + ":");
+        printResult(searchResult);
+
+    }
+
+    static void printResult(String[] arrayToShow) {
+        if (arrayToShow.length == 0) System.out.println("Ничего не найдено!");
+        for (int i = 0; i < arrayToShow.length; i++) {
+            System.out.println(arrayToShow[i]);
         }
     }
 
-    private static String[][] converterStringArray(String stringToConvert) throws ArraySizeException {
-        String[][] result = new String[MATRIX_SIZE][MATRIX_SIZE];
-        String[] checkLinesString = stringToConvert.split("\n");
-        String[] checkColumnsString;
-        if (checkLinesString.length == MATRIX_SIZE) {
-            for (int i = 0; i < checkLinesString.length; i++) {
-                checkColumnsString = checkLinesString[i].split(" ");
-                if (checkColumnsString.length == MATRIX_SIZE) result[i] = checkColumnsString;
-                else throw new ArraySizeException("Неверное количество чисел в строке. Должно быть " + MATRIX_SIZE,i,checkColumnsString.length);
-            }
-        }
-        else throw new ArraySizeException("Неверное количество строк. Должно быть: " + MATRIX_SIZE, checkLinesString.length);
-        return result;
-    }
 
-    private static int converterArrayInt(String[][] stringMatrix) throws ArrayDataException {
-        int sum = 0;
-            for (int i = 0; i < stringMatrix.length; i++) {
-                for (int j = 0; j < stringMatrix[i].length; j++) {
-                    try {
-                        sum += Integer.parseInt(stringMatrix[i][j]);
-                    } catch (NumberFormatException e) {
-                    throw new ArrayDataException("Среди данных оказалось не число!", i, j);
-                }
-                }
-            }
-
-        return sum/2;
-    }
 
 }
